6f3c530ef7cde7f2a566f8f7257582bc60a69502
Al Khalid <114647047+alkhalidsardar@users.noreply.github.com>
2022-10-14 09:11:52 +0530

    added queryselector

--- hazy.js
+++ hazy.js
@@ -20,3 +20,32 @@ let weather = {
     const { icon, description } = data.weather[0];
       const { temp, humidity } = data.main;
     const { speed } = data.wind;
+document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".searchbar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".searchbar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("Mumbai");
